93a1fc6751fdb2f97e33210cc5e47859549bfc52
Agata Foryciarz <agataf@seungworkstation1003.princeton.edu>
2018-03-04 21:00:03 -0500

    fist commit

---
+++ src/samplers/psd_sampler.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+__doc__= """
+
+Sampling class for synapse training
+
+Nicholas Turner <nturner@cs.princeton.edu>, 2017
+"""
+import os
+import utils
+import dataprovider as dp
+import h5py
+from scipy import misc
+
+def read_file(fname):
+
+    f = h5py.File(fname)
+    d = f["/main"].value
+    f.close()
+
+    return d
+
+
+class Sampler(object):
+
+    def __init__(self, datadir, dsets=[], mode="train", patchsz=(16,320,320), resize=1):
+
+      assert mode in ["train","val","test"]
+
+      datadir = os.path.expanduser(datadir)
+
+      volnames = ["input","psd_label","psd_mask"]
+      spec = { name : patchsz for name in volnames }
+
+      self.dp = self.build_data_provider(datadir, spec, mode, dsets, resize)
+
+
+    def __call__(self, **kwargs):
+      return self.dp("random", **kwargs)
+
+
+    def build_data_provider(self, datadir, spec, mode, dsets, resize):
+
+      vdp = dp.VolumeDataProvider()
+
+      for dset in dsets:
+        vdp.add_dataset( self.build_dataset(datadir, spec, dset, resize) )
+      if mode == "train":
+          vdp.set_sampling_weights([0.5, 0.1, 0.025, 0.1, 0.1, 0.025, 0.025, 0.025, 0.1])
+      if mode == "val":
+          vdp.set_sampling_weights([0.5, 0.15, 0.05, 0.1, 0.1])
+      vdp.set_augmentor(self._aug(mode))
+      vdp.set_postprocessor(self._post())
+
+      return vdp
+
+
+    def build_dataset(self, datadir, spec, dset_name, resize):
+
+      print(dset_name)
+      img = read_file(os.path.join(datadir, dset_name + "_img.h5"))
+      psd = read_file(os.path.join(datadir, dset_name + "_mit.h5")).astype("float32")
+#      seg = read_file(os.path.join(datadir, dset_name + "_seg.h5"))
+
+      img = dp.transform.divideby(img, val=255.0, dtype="float32")
+      
+      psd[psd != 0] = 1 #Binarizing psds
+      #if (resize != 1):
+      #     img = utils.resize_image(img, resize)
+      #     psd = scipy.misc.imresize(psd, 1.0/resize, interp="bilinear")
+      #     msk = (seg == 0).astype("float32") #Boundary mask
+
+      vd = dp.VolumeDataset()
+      vd.add_raw_data(key="input",      data=img)
+      vd.add_raw_data(key="psd_label",  data=psd)
+      # vd.add_raw_data(key="psd_mask",   data=msk)
+
+      vd.set_spec(spec)
+      return vd
+
+
+    def _aug(self, mode):
+
+      aug = dp.Augmentor()
+      if mode == "train":
+        aug.append('misalign', max_trans=17.0)
+      aug.append('missing', max_sec=5, mode='mix',random_color=True)
+      aug.append('blur', max_sec=5, mode='mix')
+      if mode == "train":
+        aug.append('warp')
+        aug.append('greyscale', mode='mix')
+        aug.append('flip')
+      return aug
+
+
+    def _post(self):
+      post = dp.Transformer()
+      return post
---
+++ src/utils/utils.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+__doc__ = """
+
+Miscellaneous Utils
+
+Nicholas Turner <nturner@cs.princeton.edu>, 2017
+"""
+
+import torch
+from torch.autograd import Variable
+from scipy import misc
+
+import os, re, shutil
+import datetime
+import h5py
+import numpy as np
+
+def resize_image(img, resize):
+    img_new = np.zeros((img.shape[0], img.shape[1]/resize, img.shape[2]/resize))
+    for i in range(img.shape[0]):
+        img_new[i,:,:] = misc.imresize(img[i,:,:], 1.0/resize, interp="bilinear")
+    return img_new
+
+
+def make_required_dirs(model_dir, log_dir, fwd_dir, **params):
+
+    for d in [model_dir, log_dir, fwd_dir]:
+        if not os.path.isdir(d):
+            os.makedirs(d)
+
+
+def log_tagged_modules(module_fnames, log_dir, phase, chkpt_num=0):
+    
+    timestamp = datetime.datetime.now().strftime("%d%m%y_%H%M%S")
+
+    for fname in module_fnames:
+        basename = os.path.basename(fname)
+        output_basename = "{}_{}{}_{}".format(timestamp, phase, chkpt_num, basename)
+
+        shutil.copyfile(fname, os.path.join(log_dir, output_basename))
+
+
+def save_chkpt(model, learning_monitor, chkpt_num, model_dir, log_dir):
+
+    # Save model
+    chkpt_fname = os.path.join(model_dir, "model{}.chkpt".format(chkpt_num))
+    torch.save(model.state_dict(), chkpt_fname)
+
+    # Save learning monitor 
+    lm_fname = os.path.join(log_dir, "stats{}.h5".format(chkpt_num))
+    learning_monitor.save(lm_fname, chkpt_num)
+
+
+def create_network(model_class, model_args, model_kwargs, 
+                   chkpt_num=0, model_dir=None, **params):
+
+    net = torch.nn.DataParallel(model_class(*model_args, **model_kwargs)).cuda()
+
+    if chkpt_num > 0:
+        load_network(net, chkpt_num, model_dir)
+
+    return net
+    
+
+def load_network(model, chkpt_num, model_dir):
+
+    chkpt_fname = os.path.join(model_dir, "model{}.chkpt".format(chkpt_num))
+    model.load_state_dict(torch.load(chkpt_fname))
+    
+    return model
+
+
+def load_learning_monitor(learning_monitor, chkpt_num, log_dir):
+
+    lm_fname = os.path.join(log_dir, "stats{}.h5".format(chkpt_num))
+    learning_monitor.load(lm_fname)
+
+    return learning_monitor
+    
+
+def load_chkpt(model, learning_monitor, chkpt_num, model_dir, log_dir):
+
+    m = load_network(model, chkpt_num, model_dir)
+    
+    lm = load_learning_monitor(learning_monitor, chkpt_num, log_dir)
+
+    return m, lm   
+
+
+def iter_from_chkpt_fname(chkpt_fname):
+    """ Extracts the iteration number from a network checkpoint """
+    basename = os.path.basename(chkpt_fname)
+    return int(re.findall(r"\d+", basename)[0])
+
+
+def masks_not_empty(sample, mask_names):
+    """ Tests whether a sample has any non-masked values """
+    counts = [ sample[name].sum() for name in mask_names ]
+    return 0 in counts
+
+
+def make_variable(np_arr, requires_grad=True, volatile=False):
+    """ Creates a torch.autograd.Variable from a np array """
+    if not volatile:
+      return Variable(torch.from_numpy(np_arr.copy()), requires_grad=requires_grad).cuda()
+    else:
+      return Variable(torch.from_numpy(np_arr.copy()), volatile=True).cuda()
+
+
+def set_gpus(gpu_list):
+    """ Sets the gpus visible to this process """
+    os.environ["CUDA_VISIBLE_DEVICES"] = ",".join(gpu_list)
+
+
+def read_h5(fname):
+    
+    with h5py.File(fname) as f:
+        d = f["/main"].value
+
+    return d
+
+
+def write_h5(data, fname):
+
+    if os.path.exists(fname):
+      os.remove(fname)
+
+    with h5py.File(fname) as f:
+        f.create_dataset("/main",data=data)
+ 
+
+
