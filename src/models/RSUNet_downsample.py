311399aae0313eb966a4635e7aedc937d510f23b
Agata Foryciarz <agata@foryciarz.com>
2018-03-17 18:02:01 -0400

    Update RSUNet_downsample.py

--- src/models/RSUNet_downsample.py
+++ src/models/RSUNet_downsample.py
@@ -177,7 +177,7 @@ class Model(nn.Module):
     # in most pytorch docs. I'll follow that convention here
 
     # Downsample input
-    self.add_downsample_mod(scale_factor=(1,4,4), mode='biilinear')     
+    self.add_downsample_mod(scale_factor=(1,4,4), mode='bilinear')     
         
     # Input feature embedding without batchnorm
     fs = nfeatures[0]
@@ -219,17 +219,17 @@ class Model(nn.Module):
     self.outputdeconv = OutputModule(D_in, output_spec, ks=io_size, st=io_stride)
     print("type of self.outputdeconv", type(self.outputdeconv))
          
-    self.add_upsample_mod(scale_factor=(1,4,4), mode='biilinear')
+    self.add_upsample_mod(scale_factor=(1,4,4), mode='bilinear')
 
     #m = nn.Upsample(scale_factor=(1,2,2), mode='bilinear')
     #self.outputdeconv = m(self.outputdeconv)
     # TODO: insert upsampling here      
          
-  def add_downsample_mod(self, scale_factor=(1,4,4), mode='biilinear'):
+  def add_downsample_mod(self, scale_factor=(1,4,4), mode='bilinear'):
     setattr(self, "downsample",
             nn.AvgPool3d(scale_factor))  
 
-  def add_upsample_mod(self, scale_factor=(1,4,4), mode='biilinear'):
+  def add_upsample_mod(self, scale_factor=(1,4,4), mode='bilinear'):
     setattr(self, "upsample",
             nn.Upsample(scale_factor=scale_factor, mode=mode))      
 
