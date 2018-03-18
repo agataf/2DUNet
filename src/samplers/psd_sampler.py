e0f4a43ffec6db66004d940b7309ee9a3f22948d
Agata Foryciarz <agata@foryciarz.com>
2018-03-17 21:08:52 -0400

    Update psd_sampler.py

--- src/samplers/psd_sampler.py
+++ src/samplers/psd_sampler.py
@@ -57,8 +57,8 @@ class Sampler(object):
     def build_dataset(self, datadir, spec, dset_name, resize):
 
       print(dset_name)
-      img = read_file(os.path.join(datadir, dset_name + "_img.h5"))
-      psd = read_file(os.path.join(datadir, dset_name + "_blood.h5")).astype("float32")
+      img = read_file(os.path.join(datadir, dset_name + "_resized4_img.h5"))
+      psd = read_file(os.path.join(datadir, dset_name + "_resized4_blood.h5")).astype("float32")
 #      seg = read_file(os.path.join(datadir, dset_name + "_seg.h5"))
 
       img = dp.transform.divideby(img, val=255.0, dtype="float32")
